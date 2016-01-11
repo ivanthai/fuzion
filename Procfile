a61059d879638e1944f506ce0a221b67f4085838
Ivan Thai <ivan.thai@locationlabs.com>
2016-01-11 02:33:47 -0800

    blah

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
 web: gunicorn main:app --log-file=-
-init: python create_db.py
-upgrade: python create_db.py
--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,15 @@ import os
 app = Flask(__name__)
 redis = Redis.from_url(os.environ.get("REDIS_URL"))
 from app import views
+
+from os import path
+accounts_dir = path.join(
+    path.dirname(path.realpath(__file__)), '../accounts.txt'
+)
+with file(accounts_dir, 'r') as accounts_file:
+    for line in accounts_file.readlines():
+        print line
+        id, v_id, a_id = line.split(',')
+        redis.rpush('listings', 'listings:'+id)
+        redis.hset('listings:'+id, 'airbnb', a_id)
+        redis.hset('listings:'+id, 'vrbo', v_id)
