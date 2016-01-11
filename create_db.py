222253e1bc873e1abb96f17f784d340ca0e84ddd
Ivan Thai <ivan.thai@locationlabs.com>
2016-01-11 02:30:54 -0800

    added loggin

--- create_db.py
+++ create_db.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     )
     with file(accounts_dir, 'r') as accounts_file:
         for line in accounts_file.readlines():
+            print line
             id, v_id, a_id = line.split(',')
             redis.rpush('listings', 'listings:'+id)
             redis.hset('listings:'+id, 'airbnb', a_id)
