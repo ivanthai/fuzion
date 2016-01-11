4f6bf257e3f205a9a8cdcbaf39d9c774bb3bcb2e
Ivan Thai <ivan.thai@locationlabs.com>
2016-01-11 01:23:56 -0800

    add gitignore and moved main

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ import redis as Redis
 import os
 
 app = Flask(__name__)
-redis = Redis.from_url(os.environ.get("REDIS_URL"))
+# redis = Redis.from_url(os.environ.get("REDIS_URL"))
 from app import views
